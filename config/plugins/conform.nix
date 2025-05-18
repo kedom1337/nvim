fe0341f0f6cdbe788bf297bff260ca9356f8ea62
noah <kedom@vmcall.net>
2025-05-18 16:04:26 +0200

    fix: disable format on save due it conflicting with some formatters

--- config/plugins/conform.nix
+++ config/plugins/conform.nix
@@ -31,14 +31,6 @@
         json = [ "prettier" ];
         nix = [ "nixfmt" ];
       };
-      format_on_save.__raw = ''
-        function(bufnr)
-          if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
-            return
-          end
-          return { timeout_ms = 200, lsp_fallback = true }
-        end
-      '';
     };
   };
 }
