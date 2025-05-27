9ec6ddc90c3a32fd061ae8aea5fb903f46d66f9c
noah <kedom@vmcall.net>
2025-05-27 12:22:29 +0200

    feat: remove legacy blink option

--- config/plugins/blink.nix
+++ config/plugins/blink.nix
@@ -2,7 +2,6 @@
   plugins.blink-cmp = {
     enable = true;
     settings = {
-      appearance.use_nvim_cmp_as_default = true;
       keymap.preset = "enter";
       signature.enabled = true;
       completion = {
