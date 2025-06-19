8ab12e9ba78d30561483b5d7e54368b60889fcaa
noah <kedom@vmcall.net>
2025-06-19 18:12:39 +0200

    refactor: use flake-utils instead of flake-parts

--- flake.nix
+++ flake.nix
@@ -3,46 +3,42 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     nixvim.url = "github:nix-community/nixvim";
-    flake-parts.url = "github:hercules-ci/flake-parts";
     neovim-nightly.url = "github:nix-community/neovim-nightly-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
   };
   outputs =
-    { nixvim, flake-parts, ... }@inputs:
-    let
-      nvimConfig = {
-        useNightly = false;
-      };
-    in
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [
-        "x86_64-linux"
-        "aarch64-linux"
-        "x86_64-darwin"
-        "aarch64-darwin"
-      ];
-
-      perSystem =
-        { pkgs, system, ... }:
-        let
-          nixvimLib = nixvim.lib.${system};
-          nixvim' = nixvim.legacyPackages.${system};
-          nixvimModule = {
-            inherit system;
-            module = import ./config;
-            extraSpecialArgs = {
-              inherit inputs nvimConfig;
-            };
-          };
-          nvim = nixvim'.makeNixvimWithModule nixvimModule;
-        in
-        {
-          checks = {
-            default = nixvimLib.check.mkTestDerivationFromNixvimModule nixvimModule;
-          };
-          packages = {
-            default = nvim;
-          };
-          formatter = pkgs.nixfmt-rfc-style;
+    {
+      nixpkgs,
+      nixvim,
+      flake-utils,
+      ...
+    }@inputs:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        nvimConfig = {
+          useNightly = false;
         };
-    };
+
+        pkgs = nixpkgs.legacyPackages.${system};
+        nixvimLib = nixvim.lib.${system};
+        nixvim' = nixvim.legacyPackages.${system};
+        nixvimModule = {
+          inherit system;
+          module = import ./config;
+          extraSpecialArgs = {
+            inherit inputs nvimConfig;
+          };
+        };
+      in
+      {
+        packages = {
+          default = nixvim'.makeNixvimWithModule nixvimModule;
+        };
+        checks = {
+          default = nixvimLib.check.mkTestDerivationFromNixvimModule nixvimModule;
+        };
+        formatter = pkgs.nixfmt-rfc-style;
+      }
+    );
 }
