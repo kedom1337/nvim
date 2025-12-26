3a96a91e8c333fda112fde41d59acd94b2a5c8e1
kedom <kedom@vmcall.net>
2025-12-26 23:09:34 +0100

    chore: make tsgo the default

--- config/plugins/lsp.nix
+++ config/plugins/lsp.nix
@@ -44,12 +44,10 @@
     helm_ls.enable = true;
     tofu_ls.enable = true;
     terraformls.enable = true;
-    tsgo = {
-      enable = true;
-      activate = false;
-    };
+    tsgo.enable = true;
     vtsls = {
       enable = true;
+      activate = false;
       config.settings = {
         typescript.tsserver.maxTsServerMemory = 12288;
         vtsls.autoUseWorkspaceTsdk = true;
