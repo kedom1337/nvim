e9cfdeada7f5399ef7a17ed4e36c6bb012d0859a
kedom <kedom@vmcall.net>
2026-03-29 22:46:39 +0200

    chore: enable nightly

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
     };
   };
   outputs = {nixpkgs, ...} @ inputs: let
-    useNightly = false;
+    useNightly = true;
     eachSupportedSystem = f:
       nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed (system:
         f {
