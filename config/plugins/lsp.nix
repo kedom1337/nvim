fb26ea976de4c37f082e0a5b8e018aef4c9985bd
kedom <kedom@vmcall.net>
2026-04-16 17:14:28 +0200

    feat: use vtsls again

--- config/plugins/lsp.nix
+++ config/plugins/lsp.nix
@@ -17,11 +17,17 @@
     yamlls.enable = true;
     tailwindcss.enable = true;
     oxlint.enable = true;
-    ocamllsp.enable = true;
     dockerls.enable = true;
     helm_ls.enable = true;
     tofu_ls.enable = true;
     astro.enable = true;
-    tsgo.enable = true;
+    vtsls = {
+      enable = true;
+      activate = false;
+      config.settings = {
+        typescript.tsserver.maxTsServerMemory = 12288;
+        vtsls.autoUseWorkspaceTsdk = true;
+      };
+    };
   };
 }
